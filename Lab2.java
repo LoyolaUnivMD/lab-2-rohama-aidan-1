1de2482f37e5555422dfdf9857f71a9a6c9c5cba
Lab0 <rsmulugeta@loyola.edu>
2024-01-31 17:41:28 -0500

    First code

--- Lab2.java
+++ Lab2.java
@@ -1,13 +1,13 @@
-// Programmers:  [your names here]
+// Programmers:  Aidan and Rohama
 // Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+// Due Date: 2/04
+// Lab Assignment: lab 2
+// Problem Statement: Print 10 random lottery tickets for the user
+// Data In: user name
+// Data Out: lottery tickets and estimated price
+// Credits: Notes and stack over flow website
 
+// imported scanner decimal format and random
 import java.util.Scanner;
 import java.util.Random;
 import java.text.DecimalFormat;
@@ -16,15 +16,17 @@ class Lab2 {
     public static void main(String[] args) {
         long prize = 225938745L;
         Scanner input = new Scanner(System.in);
+        // changed price int to decimal format
         DecimalFormat dfmt = new DecimalFormat("$,###");
 
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
-        
+        // asking for userinput
         System.out.print("What's your name? ");
 
         String customerName = input.nextLine();
         int i;
+        // looping through lottery numbers
         for(i=0; i<10; i++){
             String lotteryNumbers = "";
             int c = 0;
@@ -39,9 +41,10 @@ class Lab2 {
             }
             System.out.println(lotteryNumbers);
         }
-
+        // split customer name in to parts
         String[] parts = customerName.split(" ");
         String firstName = parts[0];
+        // output every thing
         System.out.println("____________________");
         System.out.println("Goodluck "+firstName+"!");
         System.out.println("Estimated Jackpot:");
