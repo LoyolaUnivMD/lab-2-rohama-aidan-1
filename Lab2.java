c2ef8425dfdd69ab0921e995fc23347436b50470
aidansundt <aidansundt@gmail.com>
2024-01-31 17:31:45 -0500

    Algorithm

--- Lab2.java
+++ Lab2.java
@@ -10,11 +10,13 @@
 
 import java.util.Scanner;
 import java.util.Random;
+import java.text.DecimalFormat;
 
 class Lab2 {
     public static void main(String[] args) {
         long prize = 225938745L;
         Scanner input = new Scanner(System.in);
+        DecimalFormat dfmt = new DecimalFormat("$,###");
 
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
@@ -22,18 +24,28 @@ class Lab2 {
         System.out.print("What's your name? ");
 
         String customerName = input.nextLine();
+        int i;
         for(i=0; i<10; i++){
             String lotteryNumbers = "";
+            int c = 0;
             for(c=0; c<6; c++){
                 Random rand = new Random();
-                int number = rand.nextInt(99);
-                    number = rand.nextInt()
+                int intNumber = rand.nextInt(99);
+                String stringNumber = String.valueOf(intNumber);
+                if(intNumber < 10){
+                    stringNumber = "0"+stringNumber;
+                }
+                lotteryNumbers = lotteryNumbers + stringNumber + " ";
             }
+            System.out.println(lotteryNumbers);
         }
 
-        String string = "004-034556";
-        String[] parts = string.split("-");
-        System.out.println(parts);
+        String[] parts = customerName.split(" ");
+        String firstName = parts[0];
+
+        System.out.println("Goodluck "+firstName+"!");
+        System.out.println("Estimated Jackpot:");
+        System.out.println(dfmt.format(prize));
 
 
     }
